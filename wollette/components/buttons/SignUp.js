a0d96d7454f7f23d7345b550332ad801cc58ff54
Aditya <iit2022003@iiitl.ac.in>
2023-09-05 17:18:53 +0530

    wollette

---
+++ wollette/components/buttons/SignUp.js
@@ -0,0 +1,13 @@
+export default function SignUp() {
+  return (
+    <div>
+      <button type="button" className="inline-flex items-start gap-[4px] bg-transparent">
+        <p className="text-[#404040] font-normal leading-[150%] text-[20px] not-italic">
+          {' '}
+          New User?
+        </p>
+        <a className="text-[#0E72E8] font-normal leading-[150%] text-[20px] not-italic">Sign up</a>
+      </button>
+    </div>
+  )
+}
